05e0955cb45fb4cf26839b53cba8577f03998459
Ryan Lai <ryanlai2@illinois.edu>
2016-01-13 18:12:57 -0600

    MySQL for Slot Machine
    The MySQL code for entering the data for the symbols and probability of
    occurrence.

---
+++ slotMachine_SQL2.sql
@@ -0,0 +1,5 @@
+insert into slot_probabilities (symbol, probability) values ('C', 20);
+insert into slot_probabilities (symbol, probability) values ('7', 20);
+insert into slot_probabilities (symbol, probability) values ('1B', 20);
+insert into slot_probabilities (symbol, probability) values ('2B', 20);
+insert into slot_probabilities (symbol, probability) values ('3B', 20);
